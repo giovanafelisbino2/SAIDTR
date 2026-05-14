1a33a90711aed4dd5ce0739ca7d4f767e0da5e21
giovanafelisbino2 <giovana.felisbino@telefonica.com>
2026-05-14 15:10:45 -0300

    reestruturacao 14/05/26

--- backend/routes/chat-router.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-const router = express.Router() 
-import { criarDemanda } from '../controllers/chat-controllers.js'
-
-router.post('/demanda', criarDemanda)
-
-export default router 
